706bc53dab57486d3b7bc67ddfba1dd78c9fc747
Sirena Gandy <sirena@sirenaalyce.com>
2021-02-14 01:56:37 -0500

    setup main and home components

---
+++ components/MainComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Main extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Placeholder</Text>
+            </View>
+        );
+    }
+}
+
+export default Main;
