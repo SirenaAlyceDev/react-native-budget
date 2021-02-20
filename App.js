b1161bf183f38947732d46136467111b204370b4
Sirena Gandy <sirena@sirenaalyce.com>
2021-02-20 12:43:31 -0500

    added react navigation dependencies

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
-import { StatusBar } from "expo-status-bar";
+import 'react-native-gesture-handler';
 import React from "react";
 import { View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
 import Main from "./components/MainComponent";
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Main />
     </View>
+    </NavigationContainer>
   );
 }
 
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import { ListItem } from 'react-native-elements';
 import HomeComponent from './HomeComponent';
-import BudgetChoice from './BudgetChoiceComponent';
+// import BudgetChoice from './BudgetChoiceComponent';
 
 class Main extends Component {
     render() {
         return (
             <View>
-                {/* <HomeComponent /> */}
-                <BudgetChoice />
+                <HomeComponent />
+                {/* <BudgetChoice /> */}
             </View>
         );
     }
